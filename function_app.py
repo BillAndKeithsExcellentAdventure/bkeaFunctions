209c103e6f82fcaeaebe1c206a6f137e96918912
KeithBertram <Keith.M.Bertram@gmail.com>
2025-03-14 10:23:54 -0400

    Fix version parameter check in Configuration function

--- function_app.py
+++ function_app.py
@@ -10,7 +10,7 @@ def Configuration(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     version = req.params.get('version')
-    if not name:
+    if not version:
         try:
             req_body = req.get_json()
         except ValueError:
